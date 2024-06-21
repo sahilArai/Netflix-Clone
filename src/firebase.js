d54b3c387bb85d0c766b138ca8a63d0cc4965b11
sahilArai <sahilarai12@gmail.com>
2024-06-21 12:17:05 +0530

    fix error

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,11 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyC1m8-5Sy9ue89LzLRSAUZzHD-dEv9l_MU",
-    authDomain: "netfixclone-affd6.firebaseapp.com",
-    projectId: "netfixclone-affd6",
-    storageBucket: "netfixclone-affd6.appspot.com",
-    messagingSenderId: "668198464693",
-    appId: "1:668198464693:web:e7601617c910f931dde2fc",
-    measurementId: "G-GT1BWTD41P"
+import { initializeApp } from "firebase/app";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBdtUe_jNQe0v-rw8X8OVe7jvTn3KnwiDg",
+    authDomain: "react-netflix-clone-650cb.firebaseapp.com",
+    projectId: "react-netflix-clone-650cb",
+    storageBucket: "react-netflix-clone-650cb.appspot.com",
+    messagingSenderId: "50014013653",
+    appId: "1:50014013653:web:7fb4e3d3f2c4eb9a84fa5c"
   };
+  const app = initializeApp(firebaseConfig);
